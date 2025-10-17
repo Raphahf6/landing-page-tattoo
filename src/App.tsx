e1105a55693ddf48e16868328f0184cd80281d3f
MENEZES LIMA <raphael.menezeslima@dxc.com>
2025-10-17 17:49:37 -0300

    altera hero

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PaginaPrincipal from './pages/PaginaPrincipal'; // Importa a página que acabamos de criar
 import PaginaSucesso from './pages/PaginaSucesso';   // Importa a página de sucesso
 
+
 export default function App() {
   return (
     <div className="min-h-screen bg-[#121212]" style={{ fontFamily: 'Inter, sans-serif' }}>
